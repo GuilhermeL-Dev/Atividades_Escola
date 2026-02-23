6ba06db95093e10c80074532450a87efe9ed6af3
Guilherme Dev <guilherme.l.dev@gmail.com>
2026-02-23 08:58:33 -0300

    Projeto02 Fase 03 Finalizado

--- Projeto 02/Projeto02Fase03SLN/Projeto02Fase03/Program.cs
+++ Projeto 02/Projeto02Fase03SLN/Projeto02Fase03/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        TransacaoPix meuPix = new TransacaoPix();
+        meuPix.Valor = 15000;
+        meuPix.ChavePix = "contato@emini.com";
+
+        TransacaoTed meuTed = new TransacaoTed();
+        meuTed.Valor = 6000;
+
+        Console.WriteLine($"PIX de R$ {meuPix.Valor} é válido? {meuPix.Validar()}");
+        Console.WriteLine($"TED de R$ {meuTed.Valor} é válido? {meuTed.Validar()}");
+    }
+}
