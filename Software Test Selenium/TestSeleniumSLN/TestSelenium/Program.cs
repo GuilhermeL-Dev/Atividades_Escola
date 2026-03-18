4f80b40e8ebe2ea75898e2668920e8f1c03b0d3d
Guilherme Dev <guilherme.l.dev@gmail.com>
2026-03-18 19:02:30 -0300

    Aplicação Test Selenium

---
+++ Software Test Selenium/TestSeleniumSLN/TestSelenium/Program.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+// 1. Início do Programa
+Console.WriteLine("Iniciando o teste de Front-end...");
+
+// 2. Configura o Driver (Abre o Chrome)
+IWebDriver driver = new ChromeDriver();
+
+try
+{
+    // 3. Navegação: Vai para o site desejado
+    driver.Navigate().GoToUrl("https://www.google.com");
+    Console.WriteLine("Página aberta: " + driver.Title);
+
+    // 4. Localização: Acha a barra de pesquisa pelo nome 'q'
+    IWebElement campoBusca = driver.FindElement(By.Name("q"));
+
+    // 5. Ação: Digita o texto e envia (Enter)
+    campoBusca.SendKeys("Neymar");
+    campoBusca.Submit();
+
+    // 6. Verificação
+    System.Threading.Thread.Sleep(2000);
+    Console.WriteLine("Busca realizada com sucesso!");
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Ocorreu um erro: " + ex.Message);
+}
+
